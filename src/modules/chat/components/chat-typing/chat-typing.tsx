e14e6aef55496d74db5b5eef21e38c107feb6030
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2025-08-16 20:40:26 -0300

    nombre de usuario añadido al evento de escribiendo

--- src/modules/chat/components/chat-typing/chat-typing.tsx
+++ src/modules/chat/components/chat-typing/chat-typing.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react"
 
 
 export const ChatTyping = () => {
-    const [isWritting, setIsWritting] = useState(false);
+    const [isWritting, setIsWritting] = useState({isWritting: false, username: ''});
 
-    const handleNewAlert = (e: { writting: boolean }) => {
+    const handleNewAlert = (e: { writting: boolean, username: string }) => {
         const writting = e.writting;
-        setIsWritting(() => writting)
+        const username = e.username;
+        setIsWritting(() => ({isWritting: writting, username}))
     }
+
 
     useEffect(() => {
         wsClient.on("on_writting", handleNewAlert);
@@ -19,9 +21,9 @@ export const ChatTyping = () => {
 
     return (
         <>
-            {isWritting ?(
+            {isWritting.isWritting ? (
                 <div className="flex gap-4 items-center justify-content-center">
-                    <span>Alguien esta escribiendo algo</span>
+                    <span>{isWritting.username} esta escribiendo algo</span>
                     <div className="flex items-center gap-1 h-4">
                         <span className="w-1.5 h-1.5 bg-gray-600 rounded-full animate-[bounce_1.4s_infinite]"></span>
                         <span className="w-1.5 h-1.5 bg-gray-600 rounded-full animate-[bounce_1.4s_infinite] animation-delay-[0.2s]"></span>
